7ae3c984c9a09675972b19d5bf25a596fdf45c7f
Abhishekamrute1 <abhishekamrute32@gmail.com>
2024-01-18 20:21:22 +0530

    hii

---
+++ js1.js
@@ -0,0 +1,30 @@
+
+// var a = 5
+// console.log(a)
+// var vs let vs const
+// vai we use for rea declare and rea assine to variabal
+// var a = 5
+// var a = 10
+// let we use for one time declaretion and many time assin the valu of variabale
+// let a =5
+// a = 60
+//const we use for one time declaration and assine the valu
+// const a = 6
+// console.log(a)
+
+
+
+
+// var firestname = 'abhishek'
+// var lastName= "amrute"
+//  console.log(firestname+"  "+lastName)
+//  console.log(`firestname ${firestname}  lastName ${lastName}`)
+
+// = vs ==
+// var a = 100;
+// var to = a == 10;
+// console.log(to);
+
+// == valu cmperision
+// === data type and valu comperision
+
